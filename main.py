2a5a5f6c62fde33c86e89ab51c8efabb5fc10309
weisong2022 <weisong2022@gmail.com>
2025-05-21 16:50:37 +0800

    7

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def greet(name):
     print(message)
 
 
+def calculate_sum2(a, b):
+    result = a + b
+    return result
+
+
 numbers = [1, 2, 3, 4, 5]
 squared_numbers = [x**2 for x in numbers]
 print(squared_numbers)
