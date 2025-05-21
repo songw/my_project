a28ab20ce146e78db33c0d6444bbbcba05740dbf
weisong2022 <weisong2022@gmail.com>
2025-05-21 16:43:43 +0800

    3

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-def calculate_sum(a,b):
-    result=a+b
+def calculate_sum(a, b):
+    result = a + b
     return result
 
+
 def greet(name):
-    message="Hello, "+name+"!"
+    message = "Hello, " + name + "!"
     print(message)
 
-numbers=[1,2,3,4,5]
-squared_numbers=[x**2 for x in numbers]
-print(squared_numbers)
+
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = [x**2 for x in numbers]
 print(squared_numbers)
