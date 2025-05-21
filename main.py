8b9b320f503289ca7c002f7de5e89d830379ee8f
weisong <mr.songw@gmail.com>
2025-05-21 16:20:11 +0800

    pre-commit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+def calculate_sum(a,b):
+    result=a+b
+    return result
+
+def greet(name):
+    message="Hello, "+name+"!"
+    print(message)
+
+numbers=[1,2,3,4,5]
+squared_numbers=[x**2 for x in numbers]
+print(squared_numbers)
